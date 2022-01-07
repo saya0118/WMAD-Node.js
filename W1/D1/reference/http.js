3713c6089b893ce1d4620a8ca7b0ceea0560a3ac
saya0118 <chemi0802@gmail.com>
2022-01-07 13:34:31 -0800

    first commit

---
+++ W1/D1/reference/http.js
@@ -0,0 +1,9 @@
+const http = require('http')
+
+//create server object
+http.createServer((req, res) => {
+    //write a response
+    res.write('Hello World')
+    res.end()
+}).listen(5000, () => console.log("Server is running!"))
+
